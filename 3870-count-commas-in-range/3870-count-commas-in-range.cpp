ecb05a301ab5eb71b18565067cdaeb979245b3a0
Adarsh Pandey <pandeyadarsh688@gmail.com>
2026-09-08 23:58:56 +0530

    Time: 0 ms (100%), Space: 8.5 MB (51.38%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
     int countCommas(int n) {
-        int count=0;
-        for(int i=1;i<=n;i++){
-            if(i/500>=2){
-                count++;
-            }
-        }
-        return count;
+     return max(0 , n-999);   
     }
 };
