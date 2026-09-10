22824fab255bd58fc97bd2ab1bd6afb4b76c2bba
Adarsh Pandey <pandeyadarsh688@gmail.com>
2026-09-11 00:35:22 +0530

    Time: 0 ms (100%), Space: 24.3 MB (68.41%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -11,22 +11,46 @@
  */
 class Solution {
 public:
-    void inorderstore(TreeNode* root,vector<int>&inorder){
+    // void inorderstore(TreeNode* root,vector<int>&inorder){
+    //     if(root==NULL){
+    //         return;
+    //     }
+    //     //LNR
+    //     inorderstore(root->left,inorder);
+    //     inorder.push_back(root->val);
+    //     inorderstore(root->right,inorder);
+
+    // }
+
+    //dusra solution
+    void solve(TreeNode*root,int k,int &count,int &ans){
         if(root==NULL){
             return;
         }
         //LNR
-        inorderstore(root->left,inorder);
-        inorder.push_back(root->val);
-        inorderstore(root->right,inorder);
+        //L
+        solve(root->left,k,count,ans);
+        //N
+        count++;
+        if(count==k){
+            ans=root->val;
+        }
+        //R
+        solve(root->right,k,count,ans);
 
     }
 
 
     int kthSmallest(TreeNode* root, int k) {
-        vector<int>inorder;
-        inorderstore(root,inorder);
-        return inorder[k-1];
+        // vector<int>inorder;
+        // inorderstore(root,inorder);
+        // return inorder[k-1];
         
+
+        //dusra solution
+        int count=0;
+        int ans;
+        solve(root,k,count,ans);
+        return ans;
     }
 };
